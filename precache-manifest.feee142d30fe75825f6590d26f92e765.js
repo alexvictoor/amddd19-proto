1654160ff5c7a5e9b1909371bf6d74129e9dd35f
Alexandre Victoor <alexandre.victoor@fluo.com>
2019-05-30 22:47:11 +0200

    Updates

---
+++ precache-manifest.feee142d30fe75825f6590d26f92e765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd120daac73d54488872bdcb0928182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c58e4ba088e225edef",
+    "url": "/static/css/main.d1204cc1.chunk.css"
+  },
+  {
+    "revision": "e42872b4d33d1e6bc128",
+    "url": "/static/js/2.631be4f3.chunk.js"
+  },
+  {
+    "revision": "12c58e4ba088e225edef",
+    "url": "/static/js/main.59a08e44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
